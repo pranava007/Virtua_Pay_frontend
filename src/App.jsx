428a6a85eb767604c7e214256fa3e6beac1a1c1d
pranava007 <pranavamuthu000@gmail.com>
2026-04-21 08:13:18 +0530

    addd

--- src/App.jsx
+++ src/App.jsx
@@ -2,44 +2,52 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 
-import HomePage from "../src/pages/Home";
+import HomePage from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import PaymentPage from "./pages/PaymentPage";
 import SettingsPage from "./pages/SettingsPage";
-import ProtectedRoute from "./components/ProtectedRoute";
 import AdminDashboard from "./pages/AdminDashboard";
+// import HistoryPage from "./pages/HistoryPage"; // ✅ add this
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-  <Routes>
-  {/* Common */}
-  <Route path="/login" element={<LoginPage />} />
-  <Route path="/register" element={<RegisterPage />} />
+    <Provider store={store}> {/* ✅ Redux */}
+      <Router> {/* ✅ Router */}
+        <Routes>
 
-  {/* USER */}
-  <Route path="/" element={<HomePage />} />
-  <Route path="/history" element={<HistoryPage />} />
+          {/* Common */}
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
 
-  {/* ADMIN */}
-  <Route 
-    path="/admin" 
-    element={
-      <ProtectedRoute roles={["admin"]}>
-        <AdminDashboard />
-      </ProtectedRoute>
-    } 
-  />
+          {/* USER */}
+          <Route path="/" element={<HomePage />} />
+          {/* <Route path="/history" element={<HistoryPage />} /> */}
+          <Route path="/pay" element={<PaymentPage />} />
 
-  <Route 
-    path="/settings" 
-    element={
-      <ProtectedRoute roles={["admin"]}>
-        <SettingsPage />
-      </ProtectedRoute>
-    } 
-  />
-</Routes>
+          {/* ADMIN */}
+          <Route 
+            path="/admin" 
+            element={
+              <ProtectedRoute roles={["admin"]}>
+                <AdminDashboard />
+              </ProtectedRoute>
+            } 
+          />
+
+          <Route 
+            path="/settings" 
+            element={
+              <ProtectedRoute roles={["admin"]}>
+                <SettingsPage />
+              </ProtectedRoute>
+            } 
+          />
+
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
